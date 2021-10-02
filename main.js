2152dd43f392d38bf78d7d7a3e5bebdff534073a
Artyom Prysiazhnyy <77559081+ArtyomPrisyazhnyy@users.noreply.github.com>
2021-10-02 19:44:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+$(function(){
+
+    /* Fixed Header */
+
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    
+
+    $(window).on("scroll load resize", function(){
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        if( scrollPos > introH ){
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    });
+
+
+    /* Smooth scroll*/
+    
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+
+        let elementID = $(this).data('scroll');
+        let elementOffSet = $(elementID).offset().top;
+
+
+        console.log(elementOffSet);
+
+        $("html, body").animate({
+            scrollTop: elementOffSet - 80
+        }, 1000)
+    })
+
+    /* Nav Toggle */
+    
+    /*пример ютубера
+        $("#navToggle").on("click resize", function(event){
+            event.preventDefault();
+
+            nav.toggleClass("showNav");
+            $(".burger__item").toggleClass("showBurger");
+        })
+    */
+
+ /*моя попытка №1*/
+
+    $('#navToggle').on("click", function() {
+        $('.nav').toggleClass("showNav");
+        $('.nav').slideToggle(200);
+        
+        $('.showNav').css('display', 'flex');
+        $(".burger__item").toggleClass("showBurger");
+    });
+
+    let doc_w = window.innerWidth;
+    $(window).on("load resize", function(){
+         let doc_w = window.innerWidth;
+        console.log(doc_w);
+
+        if(doc_w > 992){
+            $(".nav").css('display', 'flex');
+        } else{
+            $(".nav").css('display', 'none');
+            $(".showNav").css('display', 'flex');
+        }
+      
+        //         $(".burger__item").removeClass('showBurger');
+        //         if(doc_w > 977){
+        //             $(".nav").css('display', 'flex');
+        //         } else{
+        //             $(".nav").css('display', 'none');
+        //         }
+
+    })
+//   моя попытка№1 конец
+
+    /*Reviews*/
+    let slider = $("#reviewsSlider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false
+      });
+
+});
